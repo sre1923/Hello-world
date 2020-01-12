3fb6394f8fddf0789b7727df8446e72dc49543cc
sree <sree.1923@gmail.com>
2020-01-12 12:24:55 -0600

    aaaaaa

--- Shortcut.py
+++ Shortcut.py
@@ -6,7 +6,7 @@ class Graph():
     def __init__(self):
         """
         self.edges is a dict of all possible next nodes
-        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
+   ', 'C', 'E'], ...}
   l the weights between two nodes,
         with the two nodes as a tuple as the key
         e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
